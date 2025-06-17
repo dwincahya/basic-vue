4e3e663e753449b43076fb034134006411034c56
dwincahya <dwincahya8@gmail.com>
2025-06-17 15:08:16 +0700

    TemplateRef

---
+++ src/components/TemplateRef.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref, reactive, useTemplateRef } from "vue";
+
+const notes = reactive([])
+const note = ref("")
+const noteInput = useTemplateRef("noteInput");
+
+function addNote() {
+  notes.push(note.value);
+  note.value = ("");
+  noteInput.value.focus();
+}
+</script>
+
+<template>
+    <h1>Template Ref dengan Note</h1>
+    <div>
+        <input ref="noteInput" type="text" v-model="note" placeholder="Add a note" />
+        <button @click="addNote">Add Note</button>
+    </div>
+    <h1>Isi Note</h1>
+    <ul>
+        <li v-for="note in notes"> {{ note }}</li>
+    </ul>
+</template>
