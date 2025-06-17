8c8769156e6a16cdd445ecd7cea6a1a200f2357a
dwincahya <dwincahya8@gmail.com>
2025-06-17 10:21:05 +0700

    Prop in Vue

---
+++ src/components/PropsVue.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const {title = 'judul', description = 'deskripsi'} = defineProps<{
+  title: string,
+  description: string,
+}>();
+</script>
+
+<template>
+  <div class="props-component">
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+  </div>
+</template>
