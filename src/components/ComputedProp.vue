686e7a75b6ab4d8d3667a05310b4fec8b5c28076
dwincahya <dwincahya8@gmail.com>
2025-06-17 10:44:34 +0700

    Computed Props Vue

---
+++ src/components/ComputedProp.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive, computed } from 'vue';
+
+const author = reactive({
+    name: 'John Doe',
+    books: ['Vue.js Essentials', 'Advanced Vue.js', 'Vue.js for Beginners'],
+})
+
+const hasBooks = computed(() => {
+    console.log(author.books);
+return author.books.length > 0 ? 'Author has books' : 'No books available' 
+});
+
+const addBook = () => {
+    author.books.push('New Book');
+}
+</script>
+
+<template>
+    <div>
+        {{ hasBooks }}
+        <button @click="addBook">add Book</button>
+    </div>
+</template>
