64b5bf2b47b4a17592209e01e266ad8465b22907
dwincahya <dwincahya8@gmail.com>
2025-06-17 09:38:04 +0700

    Option Component

---
+++ src/components/OptionComponent.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
