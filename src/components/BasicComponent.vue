c18caf13923949a4db5df1b21f81e1bd79a957f1
dwincahya <dwincahya8@gmail.com>
2025-06-17 09:37:46 +0700

    Basic Component

---
+++ src/components/BasicComponent.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const greetings = "Hello, World!"
+</script>
+
+<template>
+    <div class="wrapper flex flex-col items-center justify-center h-screen">
+        <h1 class="text-black text-2xl">{{ greetings }}</h1>
+    </div>
+</template>
