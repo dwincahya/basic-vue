4d4d8ca9d38bfc2e37a75f0cc2edb142c6e4a4db
dwincahya <dwincahya8@gmail.com>
2025-06-17 09:37:38 +0700

    Register Global Component

---
+++ src/components/GlobalComponent.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+    const message = "This is a global component!";
+</script>
+<template>
+  <div class="wrapper flex flex-col items-center justify-center h-screen">
+    <h1 class="text-black text-2xl"> {{ message }} </h1>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import GlobalComponent from './components/GlobalComponent.vue'
+
 const app = createApp(App)
+app.component('GlobalComponent', GlobalComponent)
 
 app.use(createPinia())
 app.use(router)
