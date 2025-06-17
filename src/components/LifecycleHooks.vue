4245d062048e70f6430856dce1fec351a1307188
dwincahya <dwincahya8@gmail.com>
2025-06-17 16:34:07 +0700

    Lifecycle Hooks Vue

---
+++ src/components/LifecycleHooks.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref, onMounted, onUpdated } from 'vue'
+
+const count = ref(0)
+
+onMounted(() => {
+  console.log("Mounted: DOM tersedia");
+})
+
+onUpdated(() => {
+  console.log("Updated: count berubah ke", count.value);
+})
+</script>
+
+<template>
+  <button @click="count++">Klik: {{ count }}</button>
+</template>
