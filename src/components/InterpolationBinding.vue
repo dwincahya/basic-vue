6bed872ebf0075e108b56035419f0b67b3a41678
dwincahya <dwincahya8@gmail.com>
2025-06-17 10:20:43 +0700

    Interpolation and Binding Vue

---
+++ src/components/InterpolationBinding.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const bodyMessage = '<h1 class="text-green-600 text-2xl">Interpolation</h1>';
+const MessageAttr = {
+    id: 1,
+    class: 'message',
+};
+</script>
+<template>
+    <div v-bind="MessageAttr">{{ bodyMessage }}</div>
+    <div v-html="bodyMessage"></div>
+</template>
